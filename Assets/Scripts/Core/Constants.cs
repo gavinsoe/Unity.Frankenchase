8e7ff3605536e998c141bccec521c498fc66b0ab
Gavin Soebiantoro <gavin@kairosrenatus.com>
2015-04-24 10:25:58 +1000

    Imported Soomla Libraries

---
+++ Assets/Scripts/Core/Constants.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Constants {
+
+    // Soomla LevelUp
+    public static string lvlup_world_main = "world_main";
+    public static string lvlup_level_main = "level_main";
+    public static string lvlup_score_main = "score_main";
+}
