087a37a5406329508f6a79907be7b91c65adbceb
Gavin Soebiantoro <gavin.soebiantoro@articonnect.com>
2015-05-07 22:28:18 +1000

    Implemented most of the Elements for the Castle Environment. Updated the GameEvents script to be more versatile Updated some of the prefabs to cater to the new GameEvents Script. There might be some that have not been updated.
    Traps that have been fully tested:
    - Castle_Bat_Big
    - Castle_Bat_Small
    - Castle_Trap_Bookrack
    - Castle_Trap_Bookshelf_Death
    - Castle_Trap_Bookshelf_Slow

--- Assets/Scripts/Triggers/GameEvents.cs
+++ Assets/Scripts/Triggers/GameEvents.cs
@@ -3,56 +3,89 @@ using System.Collections;
 
 public class GameEvents : MonoBehaviour 
 {
-    public enum Effects { Speed, Death, GraveDebuff, JumpFromLeft, JumpFromRight }
-
-    public Effects effect;
-    public float effectCoefficient;
-    public float effectDuration;
+    [SerializeField] public Effect effectPrimary;
+    [SerializeField] public Effect effectSecondary;
+    public bool activateSecondaryEffect;
+    public bool destroyOnTrigger; // Whether or not to destroy the object after it has triggered
 
     private bool triggered = false; // Variable to make sure event is only triggered once.
-
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         // When in contact with player
         if (other.tag == "Player" && !triggered)
-        {
-            if (effect == Effects.Speed)
+        {   
+            if (!activateSecondaryEffect)
             {
-                // Speed up/down depending on whether it's a trap/powerup
-                //Character2D.instance.SetSpeed(effectCoefficient, effectDuration);
-                triggered = true;
-                Destroy(gameObject);
+                PlayerEvents(effectPrimary);
             }
-            else if (effect == Effects.Death)
+            else
             {
-                // Game Over
-                NavigationManager.instance.GameOver();
-                triggered = true;
+                PlayerEvents(effectSecondary);
             }
-            else if (effect == Effects.GraveDebuff)
-            {
-                // Apply or Add or Replenish debuff
-                Character2D.instance.AddDebuff();
-                triggered = true;
-            }
+            
         }
         else if (other.tag == "Frankenstein" && !triggered)
         {
-            if (effect == Effects.JumpFromLeft &&
-                Frankenstein.instance.facingRight)
+            if (!activateSecondaryEffect)
             {
-                Frankenstein.instance.jump = true;
-                triggered = true;
-                Destroy(gameObject);
+                FrankensteinEvents(effectPrimary);
             }
-            else if (effect == Effects.JumpFromRight &&
-                     Frankenstein.instance.facingRight)
+            else
             {
-                Frankenstein.instance.jump = true;
-                triggered = true;
-                Destroy(gameObject);
+                FrankensteinEvents(effectSecondary);
             }
-
         }
     }
+
+    void PlayerEvents(Effect effect)
+    {
+        if (effect.type == EffectType.Speed)
+        {
+            // Speed up/down depending on whether it's a trap/powerup
+            Character2D.instance.SetSpeed(effect.coefficient, effect.duration);
+            triggered = true;
+            if (destroyOnTrigger) Destroy(gameObject);
+        }
+        else if (effect.type == EffectType.Death)
+        {
+            // Game Over
+            NavigationManager.instance.GameOver();
+            triggered = true;
+            if (destroyOnTrigger) Destroy(gameObject);
+        }
+        else if (effect.type == EffectType.GraveDebuff)
+        {
+            // Apply or Add or Replenish debuff
+            Character2D.instance.AddDebuff();
+            triggered = true;
+            if (destroyOnTrigger) Destroy(gameObject);
+        }
+    }
+
+    void FrankensteinEvents(Effect effect)
+    {
+        if (effect.type == EffectType.JumpFromLeft &&
+            Frankenstein.instance.facingRight)
+        {
+            Frankenstein.instance.jump = true;
+            triggered = true;
+            if (destroyOnTrigger) Destroy(gameObject);
+        }
+        else if (effect.type == EffectType.JumpFromRight &&
+                 Frankenstein.instance.facingRight)
+        {
+            Frankenstein.instance.jump = true;
+            triggered = true;
+            if (destroyOnTrigger) Destroy(gameObject);
+        }
+    }
+}
+
+[System.Serializable]
+public class Effect : System.Object
+{
+    public EffectType type;
+    public float coefficient;
+    public float duration;
 }
---
+++ Assets/Scripts/Triggers/TrapTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTrigger : MonoBehaviour {
+    private Animator animator;
+
+    private bool triggered = false; // Variable to make sure event is only triggered once.
+
+    void Start()
+    {
+        animator = gameObject.GetComponentInParent<Animator>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger Enter :: " + other.tag);
+        if (other.tag == "Frankenstein" && !triggered)
+        {
+            animator.SetTrigger("Trigger");
+        }
+    }
+}
