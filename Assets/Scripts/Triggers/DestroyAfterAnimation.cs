c3691f5d430196f8882fd0baa38e09f57d629d49
Gavin Soebiantoro <gavin.soebiantoro@articonnect.com>
2015-05-14 18:53:20 +1000

    added a script to destrooy the creeps after they appear

---
+++ Assets/Scripts/Triggers/DestroyAfterAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script simply destroys an object when their animation state is "Destroy" which 
+// signifies that the animation has eneded.
+public class DestroyAfterAnimation : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (anim.GetCurrentAnimatorStateInfo(0).IsName("Destroy"))
+        {
+            Destroy(gameObject);
+        }
+	}
+}
