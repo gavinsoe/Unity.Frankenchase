0b43bf2428cbf72ee5922b919b2b7243dec6d0df
Gavin Soebiantoro <gavin.soebiantoro@articonnect.com>
2015-05-15 21:11:06 +1000

    Added Bat like behaviour to witch and triggers to set them off

--- Assets/Scripts/Core/CreepSpawner.cs
+++ Assets/Scripts/Core/CreepSpawner.cs
@@ -6,6 +6,9 @@ public class CreepSpawner : MonoBehaviour
     public GameObject batTop;
     public GameObject batMid;
     public GameObject batBot;
+    public GameObject witchTop;
+    public GameObject witchMid;
+    public GameObject witchBot;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -36,6 +39,39 @@ public class CreepSpawner : MonoBehaviour
             // Set the spawn position (should be zero)
             var spawnPosition = new Vector3(0, 0, 10);
             var bat = Instantiate(batBot);
+            bat.transform.parent = Camera.main.transform;
+            bat.transform.localPosition = spawnPosition;
+
+            // Destroy trigger to prevent event from triggering multiple times
+            Destroy(other.gameObject);
+        }
+        else if (other.tag.Equals("CreepWitchTop"))
+        {
+            // Set the spawn position (should be zero)
+            var spawnPosition = new Vector3(0, 0, 10);
+            var bat = Instantiate(witchTop);
+            bat.transform.parent = Camera.main.transform;
+            bat.transform.localPosition = spawnPosition;
+
+            // Destroy trigger to prevent event from triggering multiple times
+            Destroy(other.gameObject);
+        }
+        else if (other.tag.Equals("CreepWitchMid"))
+        {
+            // Set the spawn position (should be zero)
+            var spawnPosition = new Vector3(0, 0, 10);
+            var bat = Instantiate(witchMid);
+            bat.transform.parent = Camera.main.transform;
+            bat.transform.localPosition = spawnPosition;
+
+            // Destroy trigger to prevent event from triggering multiple times
+            Destroy(other.gameObject);
+        }
+        else if (other.tag.Equals("CreepWitchBot"))
+        {
+            // Set the spawn position (should be zero)
+            var spawnPosition = new Vector3(0, 0, 10);
+            var bat = Instantiate(witchBot);
             bat.transform.parent = Camera.main.transform;
             bat.transform.localPosition = spawnPosition;
 
