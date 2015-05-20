514c31aafae782298501efb00ddb822f5add2641
Gavin Soebiantoro <gavin.soebiantoro@articonnect.com>
2015-05-20 21:18:58 +1000

    Created a script to destroy object when we enter holding phases

---
+++ Assets/Scripts/Triggers/DestroyOnHoldPhase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnHoldPhase : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+	    if (Game.instance.currentState == GameState.HoldingPhase)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
