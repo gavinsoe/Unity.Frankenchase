ca0eb3c9e7a206ca5e80e9bb1f7cc36304f9f62c
Gavin Soebiantoro <gavin.soebiantoro@articonnect.com>
2015-08-11 09:34:10 +1000

    Adjusted how the background works and zoomed out the camera.

---
+++ Assets/Scripts/Core/BackgroundController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+    public GameObject referencedObject;
+    public float multiplier;
+    private Renderer background;
+
+	// Use this for initialization
+	void Start () {
+        background = gameObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        background.material.mainTextureOffset = new Vector2(multiplier * referencedObject.transform.position.x, background.material.mainTextureOffset.y);
+	}
+}
