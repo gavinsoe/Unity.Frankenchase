8995cf87dd07242b3f2fc167928495862dbc0c56
Gavin Soebiantoro <gavin.soebiantoro@articonnect.com>
2015-08-28 19:54:53 +1000

    Updated sword sprite, and title screen

--- Assets/Scripts/GUI/GUITitleScreen.cs
+++ Assets/Scripts/GUI/GUITitleScreen.cs
@@ -15,6 +15,18 @@ public class GUITitleScreen : GUIBaseClass
     private GameObject SoundToggleBtnObject; // A referebce to the object containing the sound toggle button
     private Toggle SoundToggleBtn; // A direct reference to the Toggle Component of the GUI object
 
+    [SerializeField]
+    private GameObject SwordToggleBtnObject;
+    private Toggle SwordToggleBtn;
+
+    [SerializeField]
+    private GameObject WhipToggleBtnObject;
+    private Toggle WhipToggleBtn;
+
+    [SerializeField]
+    private GameObject CrossbowToggleBtnObject;
+    private Toggle CrossbowToggleBtn;
+
     void Awake()
     {
         // make sure there is only 1 instance of this class.
@@ -31,6 +43,9 @@ public class GUITitleScreen : GUIBaseClass
         // Get the referenced object
         HighscoreText = HighscoreObject.GetComponent<Text>();
         SoundToggleBtn = SoundToggleBtnObject.GetComponent<Toggle>();
+        SwordToggleBtn = SwordToggleBtnObject.GetComponent<Toggle>();
+        WhipToggleBtn = WhipToggleBtnObject.GetComponent<Toggle>();
+        CrossbowToggleBtn = CrossbowToggleBtnObject.GetComponent<Toggle>();
     }
 
     void Start()
@@ -40,11 +55,24 @@ public class GUITitleScreen : GUIBaseClass
         {
             SoundToggleBtn.isOn = GameData.settingSound;
             ToggleSound();
-        } else
+        } 
+        else
         {
             ToggleSound();
         }
-            
+        
+        if (GameData.equippedWeapon == Constants.weapon_sword)
+        {
+            SwordToggleBtn.isOn = true;
+        }
+        else if (GameData.equippedWeapon == Constants.weapon_whip)
+        {
+            WhipToggleBtn.isOn = true;
+        }
+        else if (GameData.equippedWeapon == Constants.weapon_crossbow)
+        {
+            CrossbowToggleBtn.isOn = true;
+        }
     }
     
     public void UpdateHighscore()
@@ -56,12 +84,21 @@ public class GUITitleScreen : GUIBaseClass
 
     public void ToggleSound()
     {
-        
         GameData.settingSound = SoundToggleBtn.isOn;
 
         if (GameData.settingSound)
             AudioListener.volume = 1;
         else
             AudioListener.volume = 0;
+    }
+
+    public void equipWeapon(string weapon_id)
+    {
+        GameData.equippedWeapon = weapon_id;
+    }
+
+    public void upgradeWeapon(string weapon_id)
+    {
+
     }
 }
